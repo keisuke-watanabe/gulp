36d7cb128e3259897190cb8b2bc21e33642ef6ce
keisuke <k-watanabe@dji.co.jp>
2017-04-13 11:32:36 +0900

    targetDir追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,66 @@
 var gulp = require('gulp');
 var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
 var pngquant = require("imagemin-pngquant");
+var gcmq = require('gulp-group-css-media-queries');
 var plumber = require("gulp-plumber");
 var mozjpeg = require('imagemin-mozjpeg');
 var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+var cssnext = require('postcss-cssnext');
+//var excel2json = require('gulp-excel2json');
+//var csv2json = require('gulp-csv2json');
+var convertEncoding = require('gulp-convert-encoding');
+var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 
 
 
 //setup
-var website = 0;
+var website = 3;
 
 if (website === 0) {
     var rootDir = "./template/html/";
     var localUrl = "http://localhost.template/";
+    var targetDir ="/common/";
+
 
 } else if (website === 1) {
-    var rootDir = "./developjapan/html/";
+    var rootDir = "./test1/html/";
+    var targetDir ="/test/";
+
+} else if (website === 2) {
+    var rootDir = "./test2/html/";
+    var targetDir ="/test/";
+    var localUrl = "http://localhost.template2/";
+
+} else if (website === 3) {
+    var rootDir = "./test3/html/";
+    var targetDir ="/test/";
+    var localUrl = "http://localhost.template3/";
 }
 
 
-var paths = {
-    "cssSrc": rootDir + "common/css/src/*.css",
-    "imgSrc": rootDir + "common/images/src/*",
-    "imgDir": rootDir + "common/compression/images/*"
-};
+    var paths = {
+        "cssSrc": rootDir + targetDir + "css/src/*.css",
+        "sassSrc": rootDir + targetDir + "sass/*.scss",
+        "xlsxSrc": rootDir + targetDir + "xlsx/src/*.xlsx",
+        "csvSrc": rootDir + targetDir + "csv/src/*.csv",
+        "imgSrc": rootDir + targetDir + "images/src/*",
+        "imgDir": rootDir + targetDir + "compression/images/*"
+    };
+
+
+gulp.task('sass', function () {
+    var processors = [
+      cssnext()
+  ];
+    return gulp.src(paths.sassSrc)
+        .pipe(sass())
+        .pipe(postcss(processors))
+        .pipe(gcmq())
+        .pipe(gulp.dest(rootDir + targetDir + 'css/src/'));
+});
 
 
 
@@ -41,8 +75,11 @@ gulp.task('css', function () {
             suffix: '.min'
         }))
 
-    .pipe(gulp.dest(rootDir + '/common/css/'));
+        .pipe(gulp.dest(rootDir + targetDir + 'css/'));
 });
+
+
+
 
 
 
@@ -63,8 +100,37 @@ gulp.task('imgmin', function () {
        imagemin.optipng(),
        imagemin.gifsicle()
      ]))
-        .pipe(gulp.dest(rootDir + '/common/compression/images'))
+        .pipe(gulp.dest(rootDir + targetDir + 'compression/images'));
 });
+
+
+//xlsx to json
+
+/*
+gulp.task("bulid:json", function () {
+    gulp.src(paths.xlsxSrc)
+   .pipe(excel2json({
+            headRow: 1,
+            valueRowStart: 3,
+            trace: true
+        }))
+     .pipe(rename({extname: '.json'}))
+     .pipe(convertEncoding({to: "utf-8"}))
+        .pipe(gulp.dest(rootDir + '/common/json/'));
+});
+*/
+
+/*
+gulp.task("bulid:json", function () {
+    var csvParseOptions = {};
+    gulp.src(paths.csvSrc)
+        .pipe(csv2json(csvParseOptions))
+
+        .pipe(rename({extname: '.json'}))
+     .pipe(convertEncoding({to: "shift_jis"}))
+        .pipe(gulp.dest(rootDir + '/common/json/'));
+});*/
+
 
 
 gulp.task('browser-sync', function () {
@@ -72,9 +138,9 @@ gulp.task('browser-sync', function () {
         /*server: {
             baseDir: rootDir
         }*/
-    proxy: {
-      target: localUrl
-    }
+        proxy: {
+            target: localUrl
+        }
     });
 });
 
@@ -83,13 +149,14 @@ gulp.task('browser-sync', function () {
 
 // If you would like to use Sass/SCSS, switch 'less' to 'scss'.
 
-gulp.task('default', ['css', 'imgmin', 'browser-sync'], function () {
+gulp.task('default', ['css', 'imgmin', 'sass', 'browser-sync'], function () {
     //setup参照
     gulp.watch([paths.cssSrc], ['css']);
-
+    //gulp.watch([paths.xlsxSrc], ["bulid:json"])
+    //gulp.watch([paths.csvSrc], ["bulid:json"])
     //  gulp.watch([paths.tplSrc], ['html']);
     //  gulp.watch([paths.lessSrc], ['less']);
-    // gulp.watch([paths.scssSrc], ['scss']);
+    gulp.watch([paths.sassSrc], ['sass']);
 
     gulp.watch([paths.imgSrc], ['imgmin']);
 });
